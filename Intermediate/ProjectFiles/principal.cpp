3e8b50e84782182edab228de29d0f577ccdbe606
David Suyo <davidsuyob@gmail.com>
2022-10-01 11:30:05 -0400

    añadiendo biografía

--- Intermediate/ProjectFiles/principal.cpp
+++ Intermediate/ProjectFiles/principal.cpp
@@ -6,7 +6,7 @@ int main() {
 		cout << "Me llamo David Geraldino Suyo Bautista, estudio la carrera de ingenieria en ciencias d ela computacion" << endl;
 		cout << "mi numero de carnet universitario es 111-57" << endl;
 		cout << "----------------------------------------------------" << endl;
-		cout << "¿desea volver a ver el mensaje? (s/n): ";
+		cout << "¿desea volver a ver el mensaje? (s/n):  ";
 		cin >> letra;
 	} while (letra == 'S' || letra == 's');
 	return 0;
